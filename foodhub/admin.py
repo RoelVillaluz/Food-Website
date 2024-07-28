9ad56608f7c0371db78cba47da828f846ee7e2d8
RoelVillaluz <roelo.villaluz@gmail.com>
2024-07-28 16:40:05 +0800

    Customized admin interface for following models:
    - Recipe
    - Ingredient

--- foodhub/admin.py
+++ foodhub/admin.py
@@ -3,11 +3,26 @@ from .models import User, Ingredient, Step, Recipe, Profile, Allergen, Review, M
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Ingredient)
 admin.site.register(Step)
-admin.site.register(Recipe)
 admin.site.register(Profile)
 admin.site.register(Allergen)
 admin.site.register(Review)
 admin.site.register(MealPlan)
 admin.site.register(ShoppingList)
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'difficulty', 'display_allergens')
+    search_fields = ('name', 'category')
+    list_filter = ('category', 'user', 'allergens', 'difficulty')
+
+    def display_allergens(self, obj):
+        return ", ".join([allergen.name for allergen in obj.allergens.all()])
+    display_allergens.short_description = 'Allergens'
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display  = ('name', 'quantity', 'unit_of_measurement', 'recipe')
+    search_fields = ('name', 'recipe')
+    list_filter = ('name', 'recipe')
+
