838fee6f5c348f6ea0efa2454b83e1ba30dc3ca8
RoelVillaluz <roelo.villaluz@gmail.com>
2024-07-29 11:10:34 +0800

    Added RelatedObjectFilter base class for dynamic list filters
    - Introduced `RelatedObjectFilter` to handle dynamic related object filtering
    - Updated list filters to inherit from `RelatedObjectFilter` for improved flexibility-

--- foodhub/admin.py
+++ foodhub/admin.py
@@ -4,7 +4,7 @@ from django.db.models.query import QuerySet
 from django.http import HttpRequest
 from .models import User, Ingredient, Step, Recipe, Profile, Allergen, Review, MealPlan, ShoppingList
 from django.contrib.admin import SimpleListFilter
-from django.db.models import Q
+from django.db.models import Q, Count
 
 # Register your models here.
 admin.site.register(User)
@@ -12,6 +12,59 @@ admin.site.register(Step)
 admin.site.register(Profile)
 admin.site.register(Allergen)
 admin.site.register(Review)
+
+class RelatedObjectFilter(SimpleListFilter):
+    """Base class for filters that depend on the presence of related objects."""
+
+    def __init__(self, *args, **kwargs):
+        self.related_field_name = kwargs.pop('related_field_name', None)
+        self.related_model = kwargs.pop('related_model', None)
+        self.related_lookup_field = kwargs.pop('related_lookup_field', 'id')
+        self.parameter_name = kwargs.pop('parameter_name', 'related_object')
+        self.title = kwargs.pop('title', 'Related Objects')
+
+        if not self.related_field_name or not self.related_model:
+            raise ValueError("related_field_name and related_model must be provided")
+        
+        super().__init__(*args, **kwargs)
+
+    def lookups(self, request, model_admin):
+        # Get related objects based on presence of related field
+        related_objects = self.related_model.objects.filter(
+            **{f'{self.related_field_name}__isnull': False}
+        ).distinct()
+
+        return [(obj.id, str(obj)) for obj in related_objects]
+    
+    def queryset(self, request, queryset):
+        if self.value():
+            # Filter queryset based on the presence of related field
+            return queryset.filter(
+                **{f'{self.related_field_name}__{self.related_lookup_field}': self.value()}
+            )
+        return queryset
+
+class RecipeInShoppingListFilter(RelatedObjectFilter):
+    """Filter for recipes that are present in the shopping list."""
+
+    def __init__(self, *args, **kwargs):
+        kwargs['related_field_name'] = 'ingredients'
+        kwargs['related_model'] = Recipe
+        kwargs['related_lookup_field'] = 'id'
+        kwargs['parameter_name'] = 'recipe'
+        kwargs['title'] = 'Recipes in Shopping List'
+        super().__init__(*args, **kwargs)
+
+    def lookups(self, request, model_admin):
+        # Get unique recipes that are associated with any ingredient in the shopping list
+        ingredients = Ingredient.objects.filter(shoppinglist__isnull=False).values_list('recipe', flat=True).distinct()
+        recipes = Recipe.objects.filter(id__in=ingredients)
+        return [(recipe.id, recipe.name) for recipe in recipes]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(ingredients__recipe_id=self.value()).distinct()
+        return queryset
 
 
 @admin.register(Recipe)
@@ -40,24 +93,6 @@ class MealPlanAdmin(admin.ModelAdmin):
         recipes = obj.recipes.all()[:3]
         return ', '.join([recipe.name for recipe in recipes]) + ('...' if obj.recipes.count() > 3 else ' ')
     display_recipes.short_description = 'Recipes'
-
-
-class RecipeInShoppingListFilter(SimpleListFilter):
-    title = 'Recipes with ingredients in shopping list'
-    parameter_name = 'recipe'
-
-    def lookups(self, request, model_admin):
-        # Get all recipes that have ingredients in any shopping list
-        recipes = set(Ingredient.objects.filter(
-            shoppinglist__isnull=False
-        ).values_list('recipe', flat=True))
-        return [(recipe.id, recipe.name) for recipe in Recipe.objects.filter(id__in=recipes)]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            # Filter shopping lists to those containing the selected recipe
-            return queryset.filter(ingredients__recipe__id=self.value())
-        return queryset
 
 @admin.register(ShoppingList)
 class ShoppingListAdmin(admin.ModelAdmin):
