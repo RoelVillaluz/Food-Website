c348f6fd48fa3ee7101a85c31fcd096cbac793cc
RoelVillaluz <roelo.villaluz@gmail.com>
2024-07-29 09:07:22 +0800

    Updated admin interface ff:
    - Mealplans
    - Shopping LIsts

--- foodhub/admin.py
+++ foodhub/admin.py
@@ -7,8 +7,6 @@ admin.site.register(Step)
 admin.site.register(Profile)
 admin.site.register(Allergen)
 admin.site.register(Review)
-admin.site.register(MealPlan)
-admin.site.register(ShoppingList)
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
@@ -26,3 +24,28 @@ class IngredientAdmin(admin.ModelAdmin):
     search_fields = ('name', 'recipe')
     list_filter = ('name', 'recipe')
 
+@admin.register(MealPlan)
+class MealPlanAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user', 'date', 'display_recipes')
+    search_fields = ('name', 'recipes__name', 'date')
+    list_filter = ('recipes', 'user')
+
+    def display_recipes(self, obj):
+        recipes = obj.recipes.all()[:3]
+        return ', '.join([recipe.name for recipe in recipes]) + ('...' if obj.recipes.count() > 3 else ' ')
+    display_recipes.short_description = 'Recipes'
+
+@admin.register(ShoppingList)
+class ShoppingListAdmin(admin.ModelAdmin):
+    list_display = ('user', 'display_ingredients', 'display_recipes')
+
+    def display_recipes(self, obj):
+        ingredients = obj.ingredients.all()
+        recipes = set(ingredient.recipe for ingredient in ingredients)
+        return ', '.join([recipe.name for recipe in recipes]) + ('...' if len(recipes) > 3 else ' ')
+    display_recipes.short_description = 'Recipes'
+
+    def display_ingredients(self, obj):
+        ingredients = obj.ingredients.all()[:3]
+        return ', '.join([ingredient.name for ingredient in ingredients]) + ('...' if obj.ingredients.count() > 3 else ' ')
+    display_ingredients.short_description = 'Ingredients'
